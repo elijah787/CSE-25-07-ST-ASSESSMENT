93c8f28b58554f6ccdf8dd976edfb32f07aab483
Hana Debay <hana2024ug@gmail.com>
2025-10-17 13:34:10 +0300

    Signup-Page: Working on style

--- HanaDebayAlemayehu/routes/authRoutes.js
+++ HanaDebayAlemayehu/routes/authRoutes.js
@@ -4,15 +4,23 @@ const router = express.Router();
 
 const User= require("../models/userModel");
 
-router.get("/get-login", (req, res) =>{
+router.get("/login", (req, res) =>{
     res.render("login")
 });
+
+router.post("/login", async (req, res) => {
+
+})
 
 
 router.get("/signup", (req, res)=>{
     res.render("signup")
 });
 
+
+router.post("/signup", async (req, res) => {
+    
+})
 
 router.get("/sucess", (req,res) => {
     res.render("sucess-page")
