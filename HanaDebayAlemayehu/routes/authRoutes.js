077cf745e86673cce8a3d8023810eff53618e604
Hana Debay <hana2024ug@gmail.com>
2025-10-17 14:31:41 +0300

    Add login POST route for user authentication

--- HanaDebayAlemayehu/routes/authRoutes.js
+++ HanaDebayAlemayehu/routes/authRoutes.js
@@ -3,12 +3,25 @@ const path = require("path");
 const router = express.Router();
 
 const User= require("../models/userModel");
+const { error } = require("console");
 
 router.get("/login", (req, res) =>{
     res.render("login")
 });
 
 router.post("/login", async (req, res) => {
+    try {
+    const { email, password } = req.body;
+    const user = await User.findOne({email})
+    if(!user){
+        return res.render("login" ,{error: "Invalid email/phone or password"})
+    }
+    res.redirect("/sucess")
+        
+    } catch (error) {
+        console.error("Login Error", err);
+        res.render("login", {error: "Something went wrong. Please try again."})
+    }
 
 })
 
