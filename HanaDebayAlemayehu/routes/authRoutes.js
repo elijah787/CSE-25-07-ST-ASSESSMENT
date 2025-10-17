79f94fe52ad00b2fd06c4e461d724a4f2c5bc09f
Hana Debay <hana2024ug@gmail.com>
2025-10-17 14:43:39 +0300

    Add signup  POST route for user registration

--- HanaDebayAlemayehu/routes/authRoutes.js
+++ HanaDebayAlemayehu/routes/authRoutes.js
@@ -2,54 +2,57 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-const User= require("../models/userModel");
+const User = require("../models/userModel");
 const { error } = require("console");
 
-router.get("/login", (req, res) =>{
-    res.render("login")
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 router.post("/login", async (req, res) => {
-    try {
+  try {
     const { email, password } = req.body;
-    const user = await User.findOne({email})
-    if(!user){
-        return res.render("login" ,{error: "Invalid email/phone or password"})
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.render("login", { error: "Invalid email/phone or password" });
     }
-    res.redirect("/sucess")
-        
-    } catch (error) {
-        console.error("Login Error", err);
-        res.render("login", {error: "Something went wrong. Please try again."})
-    }
-
-})
-
-
-router.get("/signup", (req, res)=>{
-    res.render("signup")
+    res.redirect("/sucess");
+  } catch (error) {
+    console.error("Login Error", err);
+    res.render("login", { error: "Something went wrong. Please try again." });
+  }
 });
 
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 router.post("/signup", async (req, res) => {
-    
-})
+  try {
+    const { fullName, email, phone, password, confirmPassword } = req.body;
 
-router.get("/sucess", (req,res) => {
-    res.render("sucess-page")
+    if (password !== confirmPassword) {
+      return res.render("signup", { error: "Passwords do not match" });
+    }
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.render("signup", { error: "Email already registered" });
+    }
+    const newUser = new User({
+      fullname: fullName,
+      email,
+      phone,
+    });
+    await newUser.save();
+    res.render("signup", { success: true });
+  } catch (err) {
+    console.error("Signup Error:", err);
+    res.render("signup", { error: "Something went wrong. Please try again." });
+  }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+router.get("/sucess", (req, res) => {
+  res.render("sucess-page");
+});
 
 module.exports = router;
